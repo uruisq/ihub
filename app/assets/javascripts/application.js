3347a4486da20ff9741de7319d907015df016933
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-12-12 17:37:23 +0900

    bootstrap導入

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require cocoon
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
 //= require_tree .
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,14 +6,30 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def after_sign_in_path_for(resource)
-    home_index_path
+    feeds_path
+  end
+
+  class Forbidden < ActionController::ActionControllerError
+  end
+
+  rescue_from Exception, with: :error_500 unless Rails.env.development?
+  rescue_from Forbidden, with: :error_404
+  rescue_from AbstractController::ActionNotFound, with: :error_404 unless Rails.env.development?
+  rescue_from ActionController::RoutingError, with: :error_404 unless Rails.env.development?
+  rescue_from ActiveRecord::RecordNotFound, with: :error_404 unless Rails.env.development?
+
+  def error_500
+    render file: "#{Rails.root}/public/500.html", layout: false, status: 500
+  end
+  def error_404
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
   end
 
   protected
   def configure_permitted_parameters
     # サインアップ時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    # アカウント編集の時にnameとprofileのストロングパラメータを追加
+    # アカウント編集の時にnameのストロングパラメータを追加
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
