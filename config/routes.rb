ef9ac8ef563463b64c92de35c35315075ab2f5f1
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-12-12 10:53:49 +0900

    device導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  # ログイン、アカウント編集後、任意のページに推移させるための記述
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  get 'home/index'
+  root to: "home#index"
 end
