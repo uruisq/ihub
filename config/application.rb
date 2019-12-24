706037d8b628cadd4b2f9462115cc1936e097d6c
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-12-24 16:38:17 +0900

    js修正

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Ihub
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false
   end
 end
