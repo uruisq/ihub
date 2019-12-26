91e1bbc0598aff013d6dd029250c6dc1d9dfad21
shuhei akamine <aeiueoaoaiueo@gmail.com>
2019-12-26 19:19:43 +0900

    文字数制限の修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :content, presence: true, length: { maximum: 50 }
+  validates :content, presence: true, length: { in: 1..50 }
   
   belongs_to :user
   belongs_to :feed
